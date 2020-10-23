9a9db49b4101c39fd54e586f130c82d30a2ae576
STEIN197 <nil20122013@gmail.com>
2020-10-23 20:38:17 +0500

    Added colors scss

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -2,12 +2,12 @@
 	<ul class="list-unstyled">
 		@foreach ($links as $link)
 			<li class="{{ $link['active'] ? 'active' : '' }}">
-				<a href="{{ $link['link'] }}">{{ $link['name'] }}</a>
+				<a href="{{ $link['link'] }}" class="c-default fw-medium nolink">{{ $link['name'] }}</a>
 				@if (@$link['items'])
 					<ul class="list-unstyled">
 						@foreach ($link['items'] as $child)
 							<li class="{{ $child['active'] ? 'active' : '' }}">
-								<a href="{{ $child['link'] }}">{{ $child['name'] }}</a>
+								<a href="{{ $child['link'] }}" class="c-default fw-medium nolink">{{ $child['name'] }}</a>
 							</li>
 						@endforeach
 					</ul>
--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -1,7 +1,8 @@
 <header class="d-flex align-items-center" style="height:40px;background-color:gray">
-	<div class="container-fluid">
-		<a href="/" class="fw-bold fs-20 nolink" style="color:white">
+	<div class="container-fluid d-flex justify-content-between">
+		<a href="/" class="nolink fw-bold fs-20" style="color:white">
 			<i>mysqlviewer</i>
 		</a>
+		<a href="{{ route('logout') }}" style="color:white">Logout</a>
 	</div>
 </header>
