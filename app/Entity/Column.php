701bb53194177f2efe03a3fb60367aa59ab2c98a
Nail Gafarov <nil20122013@gmail.com>
2021-03-26 13:08:03 +0500

    Basic column creation logic

--- app/Entity/Column.php
+++ app/Entity/Column.php
@@ -16,34 +16,38 @@ class Column extends Entity {
 		DB::statement("ALTER TABLE `{$this->TABLE_SCHEMA}`.`{$this->TABLE_NAME}` DROP COLUMN `{$this->id()}`");
 	}
 
+	// TODO
 	public function update(array $data): void {
-		if (strtolower($data['DATA_TYPE']) !== $this->DATA_TYPE)
-			$this->setType($data['DATA_TYPE'], (int) $data['SIZE']);
+		if (strtolower($data['DATA_TYPE']) !== strtolower($this->DATA_TYPE))
+			$this->setType($data['DATA_TYPE']);
 		if ($data['COLUMN_DEFAULT'] !== $this->COLUMN_DEFAULT)
 			$this->setDefault($data['COLUMN_DEFAULT']);
-		if ($data['COLUMN_NAME'] && $data['COLUMN_NAME'] !== $this->COLUMN_NAME)
-			$this->rename($data['COLUMN_NAME']);
-		if ($data['AUTO_INCREMENT'])
+		if ($data['AUTO_INCREMENT'] && $this->EXTRA !== 'auto_increment')
 			$this->setAutoIncrement();
 		if ($data['COLLATION_NAME'] !== $this->COLLATION_NAME)
 			$this->setCollation($data['COLLATION_NAME']);
 		if ((bool) $data['IS_NULLABLE'] !== Util::toBool($this->IS_NULLABLE))
 			$this->setNullable((bool) $data['IS_NULLABLE']);
-		// TODO: set index/primary
+		// TODO: index
+		if ($data['COLUMN_NAME'] && $data['COLUMN_NAME'] !== $this->COLUMN_NAME)
+			$this->rename($data['COLUMN_NAME']);
 		$this->data = array_merge($this->data, $data);
 	}
 
 	private function rename(string $name): void {
+		$name = addslashes($name);
 		DB::statement("ALTER TABLE `{$this->TABLE_SCHEMA}`.`{$this->TABLE_NAME}` RENAME COLUMN `{$this->id()}` TO `{$name}`");
 		$this->COLUMN_NAME = $name;
 	}
 
-	private function setType(string $type, ?int $size = null): void {
-		DB::statement("ALTER TABLE `{$this->TABLE_SCHEMA}`.`{$this->TABLE_NAME}` MODIFY COLUMN `{$this->id()}` {$type}".($size ? "({$size})" : ''));
+	private function setType(string $type): void {
+		$type = addslashes($type);
+		DB::statement("ALTER TABLE `{$this->TABLE_SCHEMA}`.`{$this->TABLE_NAME}` MODIFY COLUMN `{$this->id()}` {$type}");
 	}
 
 	private function setDefault(string $value): void {
-		DB::statement("ALTER TABLE `{$this->TABLE_SCHEMA}`.`{$this->TABLE_NAME}` MODIFY COLUMN `{$this->id()}` DEFAULT ".(Util::isNumeric($value) ? $value : "'{$value}'"));
+		$value = addslashes($value);
+		DB::statement("ALTER TABLE `{$this->TABLE_SCHEMA}`.`{$this->TABLE_NAME}` MODIFY COLUMN `{$this->id()}` DEFAULT '{$value}'");
 	}
 
 	private function setAutoIncrement(): void {
@@ -57,13 +61,35 @@ class Column extends Entity {
 	private function setNullable(bool $value): void {
 		DB::statement("ALTER TABLE `{$this->TABLE_SCHEMA}`.`{$this->TABLE_NAME}` MODIFY COLUMN `{$this->id()}` ".($value ? 'NULL' : 'NOT NULL'));
 	}
-	
+
+	// TODO
+	private function setIndex(bool $index): void {}
+
+	// TODO
+	private function setPrimary(bool $index): void {}
+
 	public static function create(array $data): ?Column {
-		DB::statement("ALTER TABLE `{$data['schema']}`.`{$data['table']}` ADD COLUMN `{$data['COLUMN_NAME']}`");
+		if (!$data['schema'] || !$data['table'] || !$data['COLUMN_NAME'] || !$data['DATA_TYPE'])
+			return null;
+		$data = $data;
+		array_walk($data, function (&$value, $key) {
+			$value = addslashes($value);
+		});
+		$q = "ALTER TABLE `{$data['schema']}`.`{$data['table']}` ADD COLUMN `{$data['COLUMN_NAME']}` {$data['DATA_TYPE']} COLLATE `{$data['COLUMN_COLLATION']}` ";
+		$q .= $data['IS_NULLABLE'] ? 'NULL' : 'NOT NULL';
+		if ($data['EXTRA'])
+			$q .= ' AUTO_INCREMENT';
+		if ($data['COLUMN_DEFAULT'])
+			$q .= " DEFAULT '{$data['COLUMN_DEFAULT']}'";
+		DB::statement();
 		return self::read($data['COLUMN_NAME'], $data);
 	}
 
 	public static function read(string $id, array $data = []): ?Column {
+		$data = $data;
+		array_walk($data, function (&$value) {
+			$value = addslashes($value);
+		});
 		$result = DB::select("SELECT * FROM `information_schema`.`COLUMNS` WHERE `COLUMN_NAME` = '{$id}' AND `TABLE_NAME` = '{$data['table']}' AND `TABLE_SCHEMA` = '{$data['schema']}'");
 		return $result && sizeof($result) === 1 ? new self((array) $result[0]) : null;
 	}
